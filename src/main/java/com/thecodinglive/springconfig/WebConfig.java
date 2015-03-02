6fdf09e243b293c29cbad4f08e74c0fdb9376cff
sjyun <darkandark90@gmail.com>
2015-03-02 15:17:17 +0900

    webXML -> Initizlier.java

--- src/main/java/com/thecodinglive/springconfig/WebConfig.java
+++ src/main/java/com/thecodinglive/springconfig/WebConfig.java
@@ -3,21 +3,63 @@ package com.thecodinglive.springconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.thecodinglive.controller")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+	@Override
+	public void addResourceHandlers( ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/resources/*").addResourceLocations("/resources/");
+	}
+
+	/*
+	*   todo 단순 view 매핑
+	* */
+	@Override
+	public void addViewControllers( org.springframework.web.servlet.config.annotation.ViewControllerRegistry registry){
+
+		//registry.addViewController("/home").setViewName("/home");
+	}
+
+	@Override
+	public void addInterceptors( InterceptorRegistry registry){
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		registry.addInterceptor(localeChangeInterceptor);
+	}
 
 	@Bean
-	public InternalResourceViewResolver viewReslover(){
-		InternalResourceViewResolver resolver = 
-				new InternalResourceViewResolver();
+	public LocaleResolver localResolver(){
+		CookieLocaleResolver  cookieLocaleResolver = new CookieLocaleResolver();
+		cookieLocaleResolver.setDefaultLocale(StringUtils.parseLocaleString("en"));
+
+		return cookieLocaleResolver;
+	}
+
+
+
+
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver(){
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
-		
+		resolver.setViewClass(JstlView.class);
 		return resolver;
 	}
+
 }
